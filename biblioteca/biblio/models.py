8bb4c8d2bdfa4dc54119ac65c6c8c671c6d772c5
Tomas Langhi <tomas-langhi>
2020-05-15 18:15:42 -0300

    Se agregaron las clases Libro, Ejemplar y Usuario

--- biblioteca/biblio/models.py
+++ biblioteca/biblio/models.py
@@ -6,3 +6,23 @@ class Autor(models.Model):
     nombre = models.CharField(max_length=50);
     codigo = models.IntegerField(primary_key=True);
 
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=50);
+    editorial = models.CharField(max_length=50);
+    paginas =  models.IntegerField();
+    codigo = models.IntegerField(primary_key=True);
+    autor = models.ForeignKey('Autor', on_delete=models.CASCADE);
+    
+class Ejemplar(models.Model):
+    localizacion = models.CharField(max_length=50);
+    codigo = models.IntegerField(primary_key=True);
+    libro = models.ForeignKey('Libro', on_delete=models.CASCADE);
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50);
+    direccion = models.CharField(max_length=100);
+    telefono = models.CharField(max_length=20);
+    codigo = models.IntegerField(primary_key=True);
+    ejemplar = models.ManyToManyField('Ejemplar');
+
