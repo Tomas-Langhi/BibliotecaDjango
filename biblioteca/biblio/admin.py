a0a3da11c3719f468ca65e31c828cefbb2e282a2
Tomas Langhi <tomas-langhi>
2020-06-12 11:50:48 -0300

    Se modifico esteticamente el admin

--- biblioteca/biblio/admin.py
+++ biblioteca/biblio/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from biblio.models import * 
 
+class UsuarioAdmin(admin.ModelAdmin):
+
+    #Para excluir atributos
+    #exclude = ['telefono']
+    
+    #Para armar pestañas separando los datos
+    fieldsets = (
+        ('Informacion Personal',{
+            'fields': ('nombre', 'direccion', 'telefono')
+        }),
+        ('Detalle de alquileres',{
+            'fields': ('ejemplar',)
+        }),
+    )
+
+    #Para previsualizar datos
+    list_display = ['nombre', 'apellido','telefono']
+
 # Register your models here.
 admin.site.register(Autor,);
 admin.site.register(Libro,);
-admin.site.register(Usuario,);
+admin.site.register(Usuario, UsuarioAdmin,);
 admin.site.register(Ejemplar,);
